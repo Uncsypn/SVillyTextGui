202ae9e2e58a6680e5458e147a37ebf6236c3aa1
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-10 15:41:46 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,28 +1,34 @@
 local SvillyHUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
 
+-- Keep This Function For Double Text.
+local function ToFormat(color3)return"rgb("..math.floor(math.min(color3.R*255,255))..", "..math.floor(math.min(color3.G*255,255))..", "..math.floor(math.min(color3.B*255,255))..")";end
+
 -- Create HUD Window.
-local hud = SvillyHUD.new()
+getgenv().hud = SvillyHUD.new()
 
 -- Update HUD Window.
-hud:UpdateHud({
-    Text = "New HUD Text",
+getgenv().hud:UpdateHud({
+    Text = string.format("%s - <font color='%s'>%s</font>", "SVilly", ToFormat(Color3.fromRGB(100,0,100)), "HUD"),
     Color = Color3.new(1, 0, 0),
     Visible = true,
     Enabled = true
 })
 
 -- Create HUD Item.
-hud:AddItem("NewItem", {
-    Text = "New Item",
+getgenv().hud:AddItem("NewItem", {
+    Text = string.format("%s - <font color='%s'>%s</font>", "String1", ToFormat(Color3.fromRGB(100,0,0)), "String2"),
     Color = Color3.new(0, 1, 0),
     Font = Enum.Font.Arial
 })
 
 -- Update HUD Item.
-hud:UpdateItem("NewItem", {
-    Text = "Updated Text",
+getgenv().hud:UpdateItem("NewItem", {
+    Text = string.format("%s - <font color='%s'>%s</font>", "String3", ToFormat(Color3.fromRGB(100,100,0)), "String4"),
     Color = Color3.new(0, 0, 1)
 })
 
 -- Remove HUD Item.
-hud:RemoveItem("NewItem")
+getgenv().hud:RemoveItem("NewItem")
+
+-- Destroy HUD.
+getgenv().hud:DestroyHud(true)
