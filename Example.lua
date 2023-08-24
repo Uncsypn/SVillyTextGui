b5e093460d89491657200562e270e99dd98ed366
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-24 19:45:55 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,8 +1,6 @@
 -- Get The Library Loadstring.
 local HUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
 
-local function ToFormat(color3)return"rgb("..math.floor(math.min(color3.R*255,255))..", "..math.floor(math.min(color3.G*255,255))..", "..math.floor(math.min(color3.B*255,255))..")";end
-
 -- Update HUD.
 HUD:UpdateHud({
 	Shadow = true,
@@ -11,7 +9,7 @@ HUD:UpdateHud({
 	Color = Color3.fromRGB(255,255,255),
 	Font = Enum.Font.SourceSansBold,
 	RichText = true,
-	Text = string.format("%s <font color='%s'>%s</font>", "SVilly", ToFormat(Color3.fromRGB(255, 170, 255)), "Hud")
+	Text = string.format("%s <font color='%s'>%s</font>", "SVilly", UI:ToFormat(Color3.fromRGB(255, 170, 255)), "Hud")
 })
 
 -- Create HUD Item.
@@ -21,7 +19,7 @@ HUD:AddItem("Item", {
 	Color = Color3.fromRGB(255, 170, 255),
 	Font = Enum.Font.SourceSansBold,
 	RichText = true,
-	Text = string.format("%s <font color='%s'>%s</font>", "String1", ToFormat(Color3.fromRGB(255, 255, 255)), "String2")
+	Text = string.format("%s <font color='%s'>%s</font>", "String1", UI:ToFormat(Color3.fromRGB(255, 255, 255)), "String2")
 })
 
 -- Update HUD Item.
@@ -31,7 +29,7 @@ HUD:UpdateItem("Item", {
 	Color = Color3.fromRGB(0, 170, 255),
 	Font = Enum.Font.SourceSansBold,
 	RichText = true,
-	Text = string.format("%s <font color='%s'>%s</font>", "String3", ToFormat(Color3.fromRGB(255, 255, 255)), "String4")
+	Text = string.format("%s <font color='%s'>%s</font>", "String3", UI:ToFormat(Color3.fromRGB(255, 255, 255)), "String4")
 })
 
 -- Remove HUD Item.
