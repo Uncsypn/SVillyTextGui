ef29ebff97f5a8ff448c88caf836759267c14b4b
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-24 19:29:32 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,34 +1,37 @@
-local SvillyHUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
+-- Get The Library Loadstring.
+local HUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
 
--- Keep This Function For Double Text.
-local function ToFormat(color3)return"rgb("..math.floor(math.min(color3.R*255,255))..", "..math.floor(math.min(color3.G*255,255))..", "..math.floor(math.min(color3.B*255,255))..")";end
-
--- Create HUD Window.
-getgenv().hud = SvillyHUD.new()
-
--- Update HUD Window.
-getgenv().hud:UpdateHud({
-    Text = string.format("%s - <font color='%s'>%s</font>", "SVilly", ToFormat(Color3.fromRGB(100,0,100)), "HUD"),
-    Color = Color3.new(1, 0, 0),
-    Visible = true,
-    Enabled = true
+-- Update HUD.
+UpdateHud({
+	Shadow = true,
+	Logo = false,
+	Enabled = true,
+	Color = Color3.fromRGB(255,255,255),
+	Text = string.format("%s <font color='%s'>%s</font>", "SVilly", ToFormat(Color3.fromRGB(255, 170, 255)), "Hud")
 })
 
 -- Create HUD Item.
-getgenv().hud:AddItem("NewItem", {
-    Text = string.format("%s - <font color='%s'>%s</font>", "String1", ToFormat(Color3.fromRGB(100,0,0)), "String2"),
-    Color = Color3.new(0, 1, 0),
-    Font = Enum.Font.Arial
+AddItem("Item", {
+	Background = true,
+	Shadow = true,
+	Color = Color3.fromRGB(255, 170, 255),
+	Font = Enum.Font.SourceSansBold,
+	RichText = true,
+	Text = string.format("%s <font color='%s'>%s</font>", "String1", ToFormat(Color3.fromRGB(255, 255, 255)), "String2")
 })
 
 -- Update HUD Item.
-getgenv().hud:UpdateItem("NewItem", {
-    Text = string.format("%s - <font color='%s'>%s</font>", "String3", ToFormat(Color3.fromRGB(100,100,0)), "String4"),
-    Color = Color3.new(0, 0, 1)
+UpdateItem("Item", {
+	Background = false,
+	Shadow = false,
+	Color = Color3.fromRGB(0, 170, 255),
+	Font = Enum.Font.SourceSansBold,
+	RichText = true,
+	Text = string.format("%s <font color='%s'>%s</font>", "String3", ToFormat(Color3.fromRGB(255, 255, 255)), "String4")
 })
 
 -- Remove HUD Item.
-getgenv().hud:RemoveItem("NewItem")
+RemoveItem("Speed")
 
 -- Destroy HUD.
 getgenv().hud:DestroyHud(true)
