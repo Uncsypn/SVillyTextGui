4b9006f2b2ec0eea7a81bc2e200eb6957fb606fb
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-24 19:51:42 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -49,7 +49,7 @@ UIPadding.PaddingTop = UDim.new(0, 15)
 local TableOfName = {}
 local TableOfItem = {}
 function UI:ToFormat(color3)return"rgb("..math.floor(math.min(color3.R*255,255))..", "..math.floor(math.min(color3.G*255,255))..", "..math.floor(math.min(color3.B*255,255))..")";end
-function HUD:Destroy() if HudText then HudText:Destroy() end end
+function UI:Destroy() if HudText then HudText:Destroy() end end
 
 function UI:UpdateHud(Table)
 	if Table and Window and Window.Parent and LogoText and LogoText.Parent then
