5b93f020e22853c4762bca1d20d577205df78dff
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-09-08 20:20:00 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,41 +1,33 @@
 -- Get The Library Loadstring.
-local HUD = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uncsypn/SVillyTextGui/main/Source.lua"))()
 
--- Update HUD.
-HUD:UpdateHud({
-	Shadow = true,
-	Logo = false,
-	Enabled = true,
-	Color = Color3.fromRGB(255,255,255),
-	Font = Enum.Font.SourceSansBold,
-	RichText = true,
-	Text = UI:DualStrings(Color3.fromRGB(255,170,255), {LLabel = "SVilly", RLabel = "Hud"}),
+-- Updates The TextGui Properties.
+UI:Refresh({
+	Shadow = false, -- Logo Text's Shadow.
+	Color = Color3.fromRGB(255, 255, 255), -- Logo Text's Color.
+	Font = Enum.Font.GothamBold, -- Logo Text's Font.
+	Text = "TEXTGUI", -- Logo Text's Text.
 })
 
--- Create HUD Item.
-HUD:AddItem("Item", {
+-- Add A New Item
+UI:Add("Item", {
+	Background = false, -- Item's Background.
+	Shadow = true, -- Item's Shadow.
+	Color = Color3.fromRGB(255, 255, 255), -- Item's Color.
+	Font = Enum.Font.Gotham, -- Item's Text Font.
+	Order = 0, -- Layout Order. (Higher Number Will Moves It Lower)
+	Text = UI:DualStrings(Color3.fromRGB(255, 170, 255), {LLabel = "String1", RLabel = "String2"}), -- Item's Text.
+})
+
+-- Update An Existing Item (Same With The Add But Update)
+UI:Update("Item", {
 	Background = true,
-	Shadow = true,
-	Color = Color3.fromRGB(255, 170, 255),
-	Font = Enum.Font.SourceSansBold,
-	RichText = true,
-	ZIndex = 1,
-	Text = UI:DualStrings(Color3.fromRGB(0,255,255), {LLabel = "String1", RLabel = "String2"}),
-})
-
--- Update HUD Item.
-HUD:UpdateItem("Item", {
-	Background = false,
 	Shadow = false,
-	Color = Color3.fromRGB(0, 170, 255),
-	Font = Enum.Font.SourceSansBold,
-	RichText = true,
-	ZIndex = 2,
-	Text = UI:DualStrings(Color3.fromRGB(255,255,0), {LLabel = "String3", RLabel = "String4"}),
+	Color = Color3.fromRGB(255, 170, 255),
+	Font = Enum.Font.GothamBold,
+	Order = 1,
+	Text = UI:DualStrings(Color3.fromRGB(255, 255, 255), {LLabel = "String3", RLabel = "String4"}),
 })
 
--- Remove HUD Item.
-HUD:RemoveItem("Item")
-
--- Destroy HUD.
-HUD:DestroyHud()
+-- Remove The Item.
+UI:Remove("Item")
